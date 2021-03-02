07aa25a53789ba7eec3dce0a5fc7feddc6311d21
Sathyapriyan Chinnathangam Nadar <nsathya1990@gmail.com>
2021-03-02 22:33:02 +0530

    creating basic react application

---
+++ src/containers/Users.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Users extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Users</h1>
+                <p>
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry.
+                </p>
+            </div>
+        );
+    }
+}
+
+export default Users;
